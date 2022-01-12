85954e38092dbeb7acef58057ebd5d62d33d3e49
Peter Backman <peter@iostream.cc>
2022-01-12 20:21:11 +0100

    Refactor async_query for simplicity

--- include/minicomps/async_query.h
+++ include/minicomps/async_query.h
@@ -42,48 +42,7 @@ public:
       {}
 
     void with_callback(std::function<void(mc::concrete_result<result_type>&&)>&& callback) {
-      auto handler = async_query_.handler_->lookup();
-      auto& receiving_component = async_query_.handler_->receiver();
-
-      if (!receiving_component || !handler) {
-        // TODO: fallback handler?
-        std::abort(); // TODO: make this behavior configurable
-      }
-
-      if (async_query_.handler_->mutual_executor()) {
-        if (receiving_component->listener)
-          receiving_component->listener->on_invoke(async_query_.owning_component_, receiving_component.get(), async_query_.msg_info_, message_type::REQUEST);
-
-        callback_result result_handler{nullptr, async_query_.owning_component_, receiving_component.get(), async_query_.msg_info_, std::move(callback)};
-        std::apply(*handler, std::tuple_cat(std::move(arguments_), std::make_tuple(std::move(result_handler))));
-      }
-      else {
-        struct request_data {
-          std::tuple<ArgumentTypes...> arguments;
-          std::function<void(mc::concrete_result<result_type>&&)> callback;
-          executor_ptr receiver_executor; // We have to capture executor as a shared_ptr to protect against lifetime issues
-          component* receiver; // Only used by the listener
-          component* sender;   // Only used by the listener
-        };
-
-        request_data request{std::move(arguments_), std::move(callback), async_query_.owning_component_->executor, async_query_.owning_component_, receiving_component.get()};
-
-        // Note: handler as captured here could become a dangling pointer if the message handler is removed/replaced
-        auto request_task = [handler] (void* data) {
-          request_data& request = *static_cast<request_data*>(data);
-          const message_info& msg_info = get_message_info(static_cast<MessageType*>(nullptr));
-
-          // The callback_result is the object that gets called by the application to return a value to the calling component. Sender and receiver
-          // is only used by the listener.
-          callback_result result_handler{std::move(request.receiver_executor), request.receiver, request.sender, msg_info, std::move(request.callback)};
-          std::apply(*handler, std::tuple_cat(std::move(request.arguments), std::make_tuple(std::move(result_handler))));
-        };
-
-        receiving_component->executor->enqueue_work(std::move(request_task), std::move(request));
-
-        if (receiving_component->listener)
-          receiving_component->listener->on_enqueue(async_query_.owning_component_, receiving_component.get(), async_query_.msg_info_, message_type::REQUEST);
-      }
+      async_query_.execute_with_callback(std::move(callback), std::move(arguments_));
     }
 
     template<typename OuterResultType, typename CallbackType>
@@ -97,6 +56,7 @@ public:
         callback(*inner_result.get_value(), std::move(outer_result));
       });
     }
+
   private:
     async_query& async_query_;
     std::tuple<ArgumentTypes...> arguments_;
@@ -111,6 +71,52 @@ public:
   }
 
 private:
+  template<typename CallbackType, typename... ArgumentTypes>
+  void execute_with_callback(CallbackType&& callback, std::tuple<ArgumentTypes...>&& arguments) {
+    auto handler = handler_->lookup();
+    auto& receiving_component = handler_->receiver();
+
+    if (!receiving_component || !handler) {
+      // TODO: fallback handler?
+      std::abort(); // TODO: make this behavior configurable
+    }
+
+    if (handler_->mutual_executor()) {
+      if (receiving_component->listener)
+        receiving_component->listener->on_invoke(owning_component_, receiving_component.get(), msg_info_, message_type::REQUEST);
+
+      callback_result result_handler{nullptr, owning_component_, receiving_component.get(), msg_info_, std::move(callback)};
+      std::apply(*handler, std::tuple_cat(std::move(arguments), std::make_tuple(std::move(result_handler))));
+    }
+    else {
+      struct request_data {
+        std::tuple<ArgumentTypes...> arguments;
+        std::function<void(mc::concrete_result<result_type>&&)> callback;
+        executor_ptr receiver_executor; // We have to capture executor as a shared_ptr to protect against lifetime issues
+        component* receiver; // Only used by the listener
+        component* sender;   // Only used by the listener
+      };
+
+      request_data request{std::move(arguments), std::move(callback), owning_component_->executor, owning_component_, receiving_component.get()};
+
+      // Note: handler as captured here could become a dangling pointer if the message handler is removed/replaced
+      auto request_task = [handler] (void* data) {
+        request_data& request = *static_cast<request_data*>(data);
+        const message_info& msg_info = get_message_info(static_cast<MessageType*>(nullptr));
+
+        // The callback_result is the object that gets called by the application to return a value to the calling component. Sender and receiver
+        // is only used by the listener.
+        callback_result result_handler{std::move(request.receiver_executor), request.receiver, request.sender, msg_info, std::move(request.callback)};
+        std::apply(*handler, std::tuple_cat(std::move(request.arguments), std::make_tuple(std::move(result_handler))));
+      };
+
+      receiving_component->executor->enqueue_work(std::move(request_task), std::move(request));
+
+      if (receiving_component->listener)
+        receiving_component->listener->on_enqueue(owning_component_, receiving_component.get(), msg_info_, message_type::REQUEST);
+    }
+  }
+
   component* owning_component_;
   const message_info& msg_info_;
 };
